a6a75ace87582dd34d4c6dcda22486830b89ec3c
Dan Pickett <dtep100@zepler.net>
2021-10-11 22:58:55 +0100

    Refactored into static functions and added source template sections

--- arduino-light-driver.ino
+++ arduino-light-driver.ino
@@ -1,23 +1,73 @@
+
+/*******************************************************************************
+* Includes
+*******************************************************************************/
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
+
+/*******************************************************************************
+* Defines
+*******************************************************************************/
 #define DATA_PIN  6
 #define NUMPIXELS 48
 #define LED_PIN 13
 #define DELAYVAL 500
+
+/*******************************************************************************
+* Local Types and Typedefs
+*******************************************************************************/
+
+/*******************************************************************************
+* Global Variables
+*******************************************************************************/
 Adafruit_NeoPixel pixels(NUMPIXELS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
+/*******************************************************************************
+* Static Function Prototypes
+*******************************************************************************/
+static void initialise_gpio(void);
+static void initialise_lights(void);
+static int get_light_mode(void);
+static void handle_light_mode(int light_mode);
 
+/*******************************************************************************
+* Static Variables
+*******************************************************************************/
+
+/*******************************************************************************
+* Global Functions
+*******************************************************************************/
 
 void setup() {
-  // put your setup code here, to run once:
-  pixels.begin();
-  pinMode(LED_PIN, OUTPUT);
+  initialise_gpio();
+  initialise_lights();
 }
 
 void loop() {
-  static int cycle_counter = 0;
+  int light_mode = get_light_mode();
+  handle_light_mode(light_mode);
+}
+
+/*******************************************************************************
+* Static Functions
+*******************************************************************************/
+
+static void initialise_gpio(void) {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+static void initialise_lights(void) {
+  pixels.begin();
+}
+
+static int get_light_mode(void) {
+  return 0;
+}
+
+static void handle_light_mode(int light_mode) {
+    static int cycle_counter = 0;
   // put your main code here, to run repeatedly:
   pixels.clear();
 
