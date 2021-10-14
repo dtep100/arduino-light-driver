003c67817dcdacd623a2a672e8948f94b16ae2e4
Dan Pickett <dtep100@zepler.net>
2021-10-14 23:01:48 +0100

    Reworked to address underflow issue upsetting colour display. Added gamma compensation, green amplitude reduction, debug logic and fading routine

--- arduino-light-driver.ino
+++ arduino-light-driver.ino
@@ -13,7 +13,8 @@
 #define DATA_PIN  6
 #define NUMPIXELS 48
 #define LED_PIN 13
-#define DELAYVAL 500
+#define DELAYVAL 50
+#define GAMMA_VALUE 192
 
 /*******************************************************************************
 * Local Types and Typedefs
@@ -25,19 +26,11 @@
 Adafruit_NeoPixel pixels(NUMPIXELS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 /*******************************************************************************
-* Static Function Prototypes
-*******************************************************************************/
-static void initialise_gpio(void);
-static void initialise_lights(void);
-static int get_light_mode(void);
-static void handle_light_mode(int light_mode);
-
-/*******************************************************************************
 * Static Variables
 *******************************************************************************/
 
 /*******************************************************************************
-* Global Functions
+* Main Functions
 *******************************************************************************/
 
 void setup() {
@@ -48,11 +41,12 @@ void setup() {
 void loop() {
   int light_mode = get_light_mode();
   handle_light_mode(light_mode);
-}
 
-/*******************************************************************************
-* Static Functions
-*******************************************************************************/
+  digitalWrite(LED_PIN, HIGH);
+  delay(DELAYVAL);
+  digitalWrite(LED_PIN, LOW);
+  delay(DELAYVAL);
+}
 
 static void initialise_gpio(void) {
   pinMode(LED_PIN, OUTPUT);
@@ -63,7 +57,10 @@ static void initialise_lights(void) {
 }
 
 static int get_light_mode(void) {
-  return 0;
+  static int cycle_counter = 0;
+  cycle_counter += 1;
+  int light_mode = (cycle_counter >> 8) & 0x7;
+  return light_mode;
 }
 
 static void handle_light_mode(int light_mode) {
@@ -71,12 +68,14 @@ static void handle_light_mode(int light_mode) {
   cycle_counter += 1;
 
   switch(light_mode) {
-    case 0:  { rotating_rainbow(cycle_counter);   }break;
-    case 1:  { rotating_colour(cycle_counter, 0);       }break;
-    case 2:  { rotating_colour(cycle_counter, 33);     }break;
-    case 3:  { rotating_colour(cycle_counter, 66);      }break;
-    case 4:  { colour_cycling(cycle_counter);     }break;
-    default: { lights_off();                      }break;
+    case 0:  { rotating_rainbow(cycle_counter);     }break;
+    case 1:  { rotating_colour(cycle_counter, 0);   }break;
+    case 2:  { rotating_colour(cycle_counter, 33);  }break;
+    case 3:  { rotating_colour(cycle_counter, 66);  }break;
+    case 4:  { colour_cycling(cycle_counter);       }break;
+    case 5:  { fading_colour(cycle_counter, 75);    }break;
+    case 6:  { rotating_rainbow(cycle_counter);     }break;
+    default: { lights_off();                        }break;
   }
 }
 
@@ -95,12 +94,27 @@ static void colour_cycling(int cycle_counter) {
   pixels.show();
 }
 
-static void rotating_colour(int cycle_counter, int rainbow_position) {
+static void fading_colour(int cycle_counter, int rainbow_position) {
+  int brightness_level = abs((cycle_counter % (100 << 1)) - 100);
+  int red_term = get_red_component(rainbow_position, brightness_level);
+  int green_term = get_green_component(rainbow_position, brightness_level);
+  int blue_term = get_blue_component(rainbow_position, brightness_level);
+
   pixels.clear();
   for(int i = 0; i < NUMPIXELS; i++) 
   {
-    int offset = (i + cycle_counter) % NUMPIXELS;
-    int brightness_level = (100 * offset) / NUMPIXELS;
+    pixels.setPixelColor(i, pixels.Color(red_term, green_term, blue_term));
+  }
+  pixels.show();
+}
+
+static void rotating_colour(int cycle_counter, int rainbow_position) {
+  pixels.clear();
+  int peak_idx = cycle_counter % NUMPIXELS;
+  for(int i = 0; i < NUMPIXELS; i++) 
+  {
+    int distance = get_distance_between(peak_idx, i);
+    int brightness_level = ((((NUMPIXELS >> 1) - distance) * 100) / (NUMPIXELS >> 1));
     int red_term = get_red_component(rainbow_position, brightness_level);
     int green_term = get_green_component(rainbow_position, brightness_level);
     int blue_term = get_blue_component(rainbow_position, brightness_level);
@@ -114,7 +128,7 @@ static void rotating_rainbow(int cycle_counter) {
   for(int i=0; i<NUMPIXELS; i++) 
   {
     int brightness_level = 100;
-    int rainbow_position = (i + cycle_counter) % 100;
+    int rainbow_position = (((i + cycle_counter) % NUMPIXELS) * 100) / NUMPIXELS;
     int red_term = get_red_component(rainbow_position, brightness_level);
     int green_term = get_green_component(rainbow_position, brightness_level);
     int blue_term = get_blue_component(rainbow_position, brightness_level);
@@ -128,28 +142,99 @@ static void lights_off() {
   pixels.show();
 }
 
+
+/*******************************************************************************
+* Utility Functions
+*******************************************************************************/
+
+static void send_debug(int index, int red_term, int green_term, int blue_term)
+{
+  static bool initialised = false;
+
+  if(!initialised) 
+  {
+    Serial.begin(115200);
+    initialised = true;
+  }
+  
+  Serial.print(index);
+  Serial.print(",");
+  Serial.print(red_term);
+  Serial.print(",");
+  Serial.print(green_term);
+  Serial.print(",");
+  Serial.print(blue_term);
+  Serial.println();
+  delay(10);
+}
+
+static int get_distance_between(int idx1, int idx2)
+{
+  int distance = abs(idx2 - idx1);
+  if (distance > (NUMPIXELS >> 1))
+  {
+    distance = NUMPIXELS - distance;
+  }
+  return distance;
+}
+
+static int gamma_adjust(int value, int gamma_8_8) 
+{
+  int distance = min(value, 255-value);
+  int compensation = (distance * gamma_8_8) >> 8u;
+  if (compensation > value)
+  {
+    value = 0;
+  }
+  else
+  {
+    value -= compensation;
+  }
+  return value;
+}
+
 static int get_red_component(int rainbow_position, int brightness_level)
 {
-  return get_colour_component(rainbow_position - 0, brightness_level);
+  int component = get_colour_component(rainbow_position - 0, brightness_level);
+  component = gamma_adjust(component, GAMMA_VALUE);
+  return component;
 }
 
 static int get_green_component(int rainbow_position, int brightness_level)
 {
-  return get_colour_component(rainbow_position - 33, brightness_level);
+  int component = get_colour_component(rainbow_position - 33, brightness_level);
+  component = gamma_adjust(component, GAMMA_VALUE);
+  /* Adjust for eye sensitivity to green */
+  component = component >> 1;
+  return component;
 }
 
 static int get_blue_component(int rainbow_position, int brightness_level)
 {
-  return get_colour_component(rainbow_position - 66, brightness_level);
+  int component = get_colour_component(rainbow_position - 66, brightness_level);
+  component = gamma_adjust(component, GAMMA_VALUE);
+  return component;
 }
 
 static int get_colour_component(int rainbow_position, int brightness_level) {
   const int max_brightness = 100;
-  const int colour_extent = 33;
+  const int max_rainbow_pos = 100;
+  const int colour_extent = max_rainbow_pos / 3;
   const int half_colour_extent = colour_extent >> 1;
-  const int max_colour_tem = 255;
+  const int max_colour_term = 255;
   int colour_term = 0;
 
+  if (rainbow_position > max_rainbow_pos)
+  {
+    rainbow_position = max_rainbow_pos;
+  }
+
+  /* Wrap around the zero point */
+  if (rainbow_position > (colour_extent << 1))
+  {
+    rainbow_position = rainbow_position - max_rainbow_pos;
+  }
+  
   /* Establish the colour weight */
   int colour_offset = abs(rainbow_position);
   if (colour_offset > colour_extent) 
@@ -158,12 +243,22 @@ static int get_colour_component(int rainbow_position, int brightness_level) {
   }
   else if (colour_offset < half_colour_extent) 
   {
-    colour_term = max_colour_tem;
+    colour_term = max_colour_term;
   }
   else 
   {
     int dimming_range_offset = colour_offset - half_colour_extent;
-    colour_term = max_colour_tem - ((max_colour_tem  * dimming_range_offset) / half_colour_extent);
+    colour_term = max_colour_term - ((max_colour_term  * dimming_range_offset) / half_colour_extent);
+  }
+
+  /* Saturate at the max value in case of overflow */
+  if (colour_term > max_colour_term) 
+  {
+    colour_term = max_colour_term;
+  }
+  if (colour_term < 0) 
+  {
+    colour_term = 0;
   }
 
   /* Scale it for the brightness level */
